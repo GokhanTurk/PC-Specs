7548bc669c31a9e401ee5d2ffa3abf2f705c701c
Gökhan Türk <ensar.gokhan@hotmail.com>
2023-03-09 17:50:23 +0300

    Path Fix

--- PC-Specs.ps1
+++ PC-Specs.ps1
@@ -1,5 +1,5 @@
 net use \\10.2.11.25
-[string]$path="\\10.2.11.25\Saglik_Yerleskesi_Evraklar\ENVANTER\Zimmet\$env:UserName" + ".txt"
+[string]$path="\\10.2.11.25\DISBTORTAK\Saglik_Yerleskesi_Evraklar\ENVANTER\Zimmet\$env:UserName" + ".txt"
 Write-Output "Bilgisayar Adi\Kullanici" | Out-File -FilePath $path -Append
 hostname | Out-File -FilePath $path -Append
 whoami | Out-File -FilePath $path -Append
