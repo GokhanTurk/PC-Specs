4ded9d25289a9bd62c7d28d415bebc55a666251f
Gökhan Türk <ensar.gokhan@hotmail.com>
2023-03-09 17:54:55 +0300

    Update PC-Specs.ps1

--- PC-Specs.ps1
+++ PC-Specs.ps1
@@ -1,13 +1,11 @@
 net use \\10.2.11.25
 [string]$path="\\10.2.11.25\DISBTORTAK\Saglik_Yerleskesi_Evraklar\ENVANTER\Zimmet\$env:UserName" + ".txt"
-Write-Output "Bilgisayar Adi\Kullanici" | Out-File -FilePath $path -Append
+Write-Output "Bilgisayar Adi" | Out-File -FilePath $path -Append
 hostname | Out-File -FilePath $path -Append
+Write-Output "Kullanici Adi" | Out-File -FilePath $path -Append
 whoami | Out-File -FilePath $path -Append
-#Write-Output "Bilgisayar Adi" | Out-File -FilePath $path -Append
-#hostname | Out-File -FilePath $path -Append
 Write-Output "Bilgisayar Modeli" | Out-File -FilePath $path -Append
 wmic /append:$path computersystem get systemfamily
-#Write-Output "Seri Nu" | Out-File -FilePath $path -Append
 wmic /append:$path bios get serialnumber
 getmac /v | Out-File -FilePath $path -Append
 Write-Output "Disk Modeli" | Out-File -FilePath $path -Append
